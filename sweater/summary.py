ecd57fcfe40967cd479fe84a72e3250166712f69
Vordazing <vor.daz@mail.ru>
2022-12-28 22:32:05 +0300

    addnew

--- sweater/summary.py
+++ sweater/summary.py
@@ -6,7 +6,7 @@ from sweater import app
 
 
 @app.route('/summary', methods=['GET', 'POST'], defaults={"page": 1})
-@app.route('/<int:page>', methods=['GET', 'POST'])
+@app.route('/<int:page>/<text:tag>', methods=['GET', 'POST'])
 def summary(page):
     page = page
     pages = 15
@@ -21,7 +21,7 @@ def summary(page):
                                                                                             Summary_all_filter.hostname.like(search), Summary_all_filter.worker.like(search), Summary_all_filter.ip_addres.like(search),
                                                                                             Summary_all_filter.model.like(search), Summary_all_filter.owner.like(search), Summary_all_filter.redirect.like(search))).paginate(per_page=pages, error_out=False)
         else:
-            saf = Summary_all_filter.query.order_by(Summary_all_filter.source).paginate(per_page=pages, error_out=False)
+            saf = Summary_all_filter.query.order_by(Summary_all_filter.source).paginate(per_page=pages,  error_out=False)
 
         return render_template('summary.html', result=saf, tag=search)
 
